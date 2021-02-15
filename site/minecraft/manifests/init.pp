42dff6dba5b606aa36818c6f350b9e00147320b6
Divya Mounika Kalluru <71857062+DivyaMounikaKalluru@users.noreply.github.com>
2021-02-15 16:47:37 -0500

    Create init.pp

---
+++ site/minecraft/manifests/init.pp
@@ -0,0 +1,25 @@
+class minecraft
+{
+file {'/opt/minecraft':
+ensure => directory,
+}
+file {'opt/minecraft/minecraft_server.jar':
+ensure => file,
+source => 'URL'
+}
+package {'java':
+ensure => present,
+}
+file {'/opt/minecraft/eula.txt':
+ensure => file,
+content => 'eula=true',
+}
+file {'/etc/systemd/system/minecraft.service':
+ensure => file
+source => 'puppet:///modules/minecraft/minecraft.service'
+}
+service {'minecraft':
+ensure => running,
+enable => true,
+}
+}
