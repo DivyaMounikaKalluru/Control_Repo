d9e9f5b7d28be3ec65aed526f2792707c7a06f20
Divya Mounika Kalluru <71857062+DivyaMounikaKalluru@users.noreply.github.com>
2021-02-16 11:20:47 -0500

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,17 +1,18 @@
-class minecraft
-{
-file {'/opt/minecraft':
+class minecraft {
+$url = 'URL',
+$install_dir = '/opt/minecraft'
+file {$install_dir:
 ensure => directory,
 }
-file {'/opt/minecraft/minecraft_server.jar':
+file {"${install_dir}/minecraft_server.jar":
 ensure => file,
-source => 'URL'
+source => $url,
 before => Service{'minecraft'},
 }
 package {'java':
 ensure => present,
 }
-file {'/opt/minecraft/eula.txt':
+file {"${install_dir}/eula.txt":
 ensure => file,
 content => 'eula=true',
 }
@@ -22,6 +23,6 @@ source => 'puppet:///modules/minecraft/minecraft.service'
 service {'minecraft':
 ensure => running,
 enable => true,
-require => {Package{'java'}, File{'/opt/minecraft/eula.txt'}, File{'/etc/systemd/system/minecraft.service'}}
+require => {Package{'java'}, File{"${install_dir}/eula.txt"}, File{'/etc/systemd/system/minecraft.service'}}
 }
 }
